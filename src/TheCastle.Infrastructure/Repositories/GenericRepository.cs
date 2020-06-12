8703be28f97fc0c5a7143222d1ccb7a43ed5ce69
Renaud Simon <48937051+Renaud1234@users.noreply.github.com>
2020-06-12 20:40:09 +0100

    Add Generic Repository in Infrastructure

---
+++ src/TheCastle.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheCastle.Infrastructure.Data;
+using TheCastle.Kernel;
+using TheCastle.Kernel.Entities.Base;
+
+namespace TheCastle.Infrastructure.Repositories
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity>
+        where TEntity : BaseEntity
+    {
+        private readonly ApplicationDBContext _dbContext;
+
+        public GenericRepository(ApplicationDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task Create(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetOne(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TEntity>> ListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/TheCastle.Kernel/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheCastle.Kernel.Entities.Base;
+
+namespace TheCastle.Kernel
+{
+    public interface IGenericRepository<TEntity>
+        where TEntity : BaseEntity
+    {
+        IQueryable<TEntity> GetAll();
+        Task<List<TEntity>> ListAll();
+        Task<TEntity> GetOne(int id);
+        Task Create(TEntity entity);
+        Task Update(TEntity entity);
+        Task Delete(int id);
+        Task Delete(TEntity entity);
+    }
+}
