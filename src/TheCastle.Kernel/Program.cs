8081142b13f636d65b0e1380f34251c030c5eb5b
Renaud Simon <48937051+Renaud1234@users.noreply.github.com>
2020-06-12 10:44:48 +0100

    Add Clean Architecture New Projects Kernel, Infrastructure, Core, Web

---
+++ src/TheCastle.Kernel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheCastle.Kernel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
