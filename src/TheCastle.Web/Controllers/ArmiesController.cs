4550cfc29c75b8033738cde9425f999f5ed6a19a
Renaud Simon <48937051+Renaud1234@users.noreply.github.com>
2020-06-15 10:37:38 +0100

    Add CRUD views + controllers for Castle and Armies entities

---
+++ src/TheCastle.Web/Controllers/ArmiesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TheCastle.Infrastructure.Data;
+using TheCastle.Kernel.Entities;
+
+namespace TheCastle.Web.Controllers
+{
+    public class ArmiesController : Controller
+    {
+        private readonly ApplicationDBContext _context;
+
+        public ArmiesController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Armies
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Armies.ToListAsync());
+        }
+
+        // GET: Armies/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var army = await _context.Armies
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (army == null)
+            {
+                return NotFound();
+            }
+
+            return View(army);
+        }
+
+        // GET: Armies/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Armies/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,Id")] Army army)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(army);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(army);
+        }
+
+        // GET: Armies/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var army = await _context.Armies.FindAsync(id);
+            if (army == null)
+            {
+                return NotFound();
+            }
+            return View(army);
+        }
+
+        // POST: Armies/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Name,Id")] Army army)
+        {
+            if (id != army.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(army);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ArmyExists(army.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(army);
+        }
+
+        // GET: Armies/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var army = await _context.Armies
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (army == null)
+            {
+                return NotFound();
+            }
+
+            return View(army);
+        }
+
+        // POST: Armies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var army = await _context.Armies.FindAsync(id);
+            _context.Armies.Remove(army);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ArmyExists(int id)
+        {
+            return _context.Armies.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ src/TheCastle.Web/Controllers/CastlesController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TheCastle.Infrastructure.Data;
+using TheCastle.Kernel.Entities;
+
+namespace TheCastle.Web.Controllers
+{
+    public class CastlesController : Controller
+    {
+        private readonly ApplicationDBContext _context;
+
+        public CastlesController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Castles
+        public async Task<IActionResult> Index()
+        {
+            var applicationDBContext = _context.Castles.Include(c => c.Army);
+            return View(await applicationDBContext.ToListAsync());
+        }
+
+        // GET: Castles/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var castle = await _context.Castles
+                .Include(c => c.Army)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (castle == null)
+            {
+                return NotFound();
+            }
+
+            return View(castle);
+        }
+
+        // GET: Castles/Create
+        public IActionResult Create()
+        {
+            ViewData["ArmyId"] = new SelectList(_context.Armies, "Id", "Id");
+            return View();
+        }
+
+        // POST: Castles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,ArmyId,Id")] Castle castle)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(castle);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ArmyId"] = new SelectList(_context.Armies, "Id", "Id", castle.ArmyId);
+            return View(castle);
+        }
+
+        // GET: Castles/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var castle = await _context.Castles.FindAsync(id);
+            if (castle == null)
+            {
+                return NotFound();
+            }
+            ViewData["ArmyId"] = new SelectList(_context.Armies, "Id", "Id", castle.ArmyId);
+            return View(castle);
+        }
+
+        // POST: Castles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Name,ArmyId,Id")] Castle castle)
+        {
+            if (id != castle.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(castle);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CastleExists(castle.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ArmyId"] = new SelectList(_context.Armies, "Id", "Id", castle.ArmyId);
+            return View(castle);
+        }
+
+        // GET: Castles/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var castle = await _context.Castles
+                .Include(c => c.Army)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (castle == null)
+            {
+                return NotFound();
+            }
+
+            return View(castle);
+        }
+
+        // POST: Castles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var castle = await _context.Castles.FindAsync(id);
+            _context.Castles.Remove(castle);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CastleExists(int id)
+        {
+            return _context.Castles.Any(e => e.Id == id);
+        }
+    }
+}
